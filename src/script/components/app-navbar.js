85ba170652983cd0360f7c1acd25537a0bd74f4e
xrenzo1 <athafaiq91@gmail.com>
2026-05-04 14:32:02 +0700

    update title and judul

--- src/script/components/app-navbar.js
+++ src/script/components/app-navbar.js
@@ -3,15 +3,15 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { searchMovie } from "../data/data-movie";
 
 class AppNavbar extends HTMLElement {
-    connectedCallback() {
-        this.render();
-    };
+  connectedCallback() {
+    this.render();
+  }
 
-    render() {
-        this.innerHTML = `
+  render() {
+    this.innerHTML = `
             <nav class="navbar navbar-light" style="background-color: #EAD196; font-family: 'Roboto Slab', serif;">
                 <div class="container-fluid">
-                    <h1 class="navbar-brand">My Movie</h1>
+                    <h1 class="navbar-brand">Cinema Info</h1>
                     <form id="liveAlertPlaceholder" class="d-flex">
                         <input id="searchInput" class="form-control me-2" type="search" placeholder="Masukkan Judulnya" aria-label="Search">
                         <button id="searchButton" class="btn btn-outline-success" type="submit">Search</button>
@@ -19,18 +19,18 @@ class AppNavbar extends HTMLElement {
                 </div>
             </nav>   
         `;
-        const searchButton = this.querySelector('#searchButton');
-        searchButton.addEventListener('click', async (e) => {
-            e.preventDefault();
-            const searchInput = this.querySelector('#searchInput');
-            const keyword = searchInput.value;
-            if (keyword.trim().length >= 3) {
-                await searchMovie(keyword);
-            } else {
-                alert('masukkan minimal 3 kata');
-            };
-        });
-    };
-};
+    const searchButton = this.querySelector("#searchButton");
+    searchButton.addEventListener("click", async (e) => {
+      e.preventDefault();
+      const searchInput = this.querySelector("#searchInput");
+      const keyword = searchInput.value;
+      if (keyword.trim().length >= 3) {
+        await searchMovie(keyword);
+      } else {
+        alert("masukkan minimal 3 kata");
+      }
+    });
+  }
+}
 
-customElements.define('app-navbar', AppNavbar);
+customElements.define("app-navbar", AppNavbar);
